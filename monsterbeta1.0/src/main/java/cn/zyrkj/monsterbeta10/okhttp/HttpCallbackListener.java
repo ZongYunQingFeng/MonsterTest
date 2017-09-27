4d97637897793ef82fa1ba6b5d9e1bcfe2a551ca
zongyunqingfeng <1145308642@qq.com>
2017-09-27 17:37:45 +0800

    MonsterBeta1.0提交，通过缓存登录，若过期则请求服务器登录，退出登录则重置登录状态

--- monsterbeta1.0/src/main/java/cn/zyrkj/monsterbeta10/okhttp/HttpCallbackListener.java
+++ monsterbeta1.0/src/main/java/cn/zyrkj/monsterbeta10/okhttp/HttpCallbackListener.java
@@ -3,5 +3,5 @@ package cn.zyrkj.monsterbeta10.okhttp;
 //定义接口
 public interface HttpCallbackListener {
     void onFinish(String response);
-    void onError(Exception e);
+    void onError(Exception e); 
 }
---
+++ monsterbeta1.0/src/main/java/cn/zyrkj/monsterbeta10/okhttp/StateCheckCallbackListener.java
@@ -0,0 +1,10 @@
+package cn.zyrkj.monsterbeta10.okhttp;
+
+/**
+ * Created by Administrator on 2017/9/26.
+ */
+
+public interface StateCheckCallbackListener {
+    void onValid();
+    void onInvalid();
+}
